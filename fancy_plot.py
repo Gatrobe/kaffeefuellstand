2be3c16ef38ad29da0cc32ddefa95013caec95dd
lemoer <git@irrelefant.net>
2019-09-17 22:55:44 +0200

    update

--- fancy_plot.py
+++ fancy_plot.py
@@ -4,14 +4,16 @@ import sqlite3
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 import numpy as np
 import requests
 from datetime import datetime, timedelta
+from adjustText import adjust_text
 
 import os
 
-API_URL = 'http://shop.gatrobe.de/api'
-COFFEE_PRODUCT_ID = 1
+API_URL = 'https://shop.gatrobe.de/api'
+COFFEE_PRODUCT_ID = 53
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -32,20 +34,18 @@ consumers = None
 def get_consumer(consumer_id):
     global consumers
     if consumers is None:
-        consumers = requests.get(API_URL + '/consumers').json()
+        consumers = requests.get(API_URL + '/users').json()['users']
 
-    return next(map(lambda x: x['name'], filter(lambda x: x['id'] == consumer_id, consumers)), None)
+    return next(map(lambda x: x['firstname'] + ' ' + x['lastname'], filter(lambda x: x['id'] == consumer_id, consumers)), None)
 
 def get_coffee_purchases():
-    purchases = requests.get(API_URL + '/purchases/50').json()
+    purchases = requests.get(API_URL + '/purchases?limit=50').json()['purchases']
     purchases = filter(lambda x: x['product_id'] == COFFEE_PRODUCT_ID, purchases)
 
     for p in purchases:
-        if p['revoked']:
-            continue
         for i in range(p['amount']):
             yield {
-                'name': get_consumer(p['consumer_id']),
+                'name': get_consumer(p['user_id']),
                 'date': datetime.strptime(p['timestamp'], '%a, %d %b %Y %X %Z')
             }
 
@@ -60,12 +60,25 @@ def plot_it(png_name, titlestr, start_datetime = None):
     for t_, s, remaining_cups, raw in c.fetchall():
         if start_datetime is not None and t_ < start_datetime:
              continue
+
+        if x is None:
+            continue
+
         t += [t_]
         x += [remaining_cups]
         r += [raw]
     
     # Data for plotting
-    fig, ax = plt.subplots()
+    #fig, (ax, ax2) = plt.subplots(2, 1, figsize=(5,10))
+    fig, ax = plt.subplots(figsize=(5,7))
+
+    # UGLY HACK: this fails due to strange reasons for the first run... 
+    try:
+        ax.xaxis_date()
+    except ValueError:
+        pass
+    ax.xaxis_date()
+
     ax.plot(t, x)
     
     ax.set(xlabel='time', ylabel='Cups',
@@ -74,27 +87,46 @@ def plot_it(png_name, titlestr, start_datetime = None):
     ax.set_yticks(range(0,9))
     ax.legend(('Cups',), loc=2)
     
-    
-    ax2 = ax.twinx()
-    ax2.plot(t, r, 'r')
-    ax2.set(ylim=(0, 1024), ylabel="Raw ADC Value")
-    ax2.legend(('Raw',), loc=1)
+    #ax2 = ax.twinx()
+    #ax2.plot(t, r, 'r')
+    #ax2.set(ylim=(0, 1024), ylabel="Raw ADC Value")
+    #ax2.legend(('Raw',), loc=1)
+    #ax2.grid()
 
+    texts = []
     y = [v or 0.0 for v in x]
 
+    t_min = None
+
+    i = 0
     for p in get_coffee_purchases():
         timestamps = list(map(to_timestamp, t))
         timestamp_now = to_timestamp(p['date'])
 
-        xy = (p['date'], np.interp(timestamp_now, timestamps, y))
-        xy_text = (p['date'], np.interp(timestamp_now, timestamps, y)+0.5)
+        new = p['date']
 
-        ax.annotate(p['name'], xy, xy_text, arrowprops=dict(arrowstyle='->'))
+        if t_min is not None and new > t_min:
+            new = t_min
+
+        t_min = new - timedelta(hours = 1)
+
+        xy = (p['date'], np.interp(timestamp_now, timestamps, y))
+        xy_text = (new, - 6) #np.interp(timestamp_now, timestamps, y)+0.5)
+        i += 1
+
+        texts += [ ax.annotate(p['name'], xy, xy_text, arrowprops=dict(arrowstyle='->', alpha=0.5, color="#00ff00"), rotation=90) ]
     
     fig.autofmt_xdate()
+    fig.subplots_adjust(bottom=0.4)
+    ax.xaxis.set_major_formatter(DateFormatter("%H:%M"))
+
+    #adjust_text(texts)
     
     fig.savefig(png_name)
+    #fig.tight_layout()
     #plt.show()
+    return texts
 
 
-plot_it('plot_fancy.png', ' (last 6h)', datetime.now() - timedelta(hours = 6))
+texts = plot_it('plot_fancy.png', ' (last 24h)', datetime.now() - timedelta(hours = 24))
+
