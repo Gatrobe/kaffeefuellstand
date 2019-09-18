0c55251fe1112f89608c084c00719fba79243b72
HayWo <47386066+HayWo@users.noreply.github.com>
2019-09-18 13:36:59 +0200

    replace old sensor code with new version

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,247 +1,200 @@
-#define SENSOR A0
+    #include "./config.h"
 
-#define KANNE_LEER 410
-#define KANNE_VOLL 605
-#define KANNE_05 590
-#define KANNE_1 583
-#define KANNE_15 578
-#define KANNE_2 554
-#define KANNE_25 539
-#define KANNE_3 525
-#define KANNE_35 510
-#define KANNE_4 496
-#define KANNE_45 481
-#define KANNE_5 468
-#define KANNE_55 453
-#define KANNE_6 439
-#define KANNE_65 424
-#define KANNE_WEG 200
+    Adafruit_NeoPixel line = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#include "wifi_password.h"
+    ESP8266WebServer server(80);
 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+    float cups = MAX_CUPS;
 
-ESP8266WebServer server(80);
-String page = "";
+    unsigned long now = 0;
+    unsigned long last_change = 0;
 
-void setup() {
-  //Serial.begin(115200);
-  
-  page = "<h1>Kaffee</h1><p><a href=\"kaffee\"><button>Fuellstand abfragen</button></a>&nbsp;</p>";
-  connect_to_wifi();
- // WiFi.begin(ssid, password);
- // while (WiFi.status() != WL_CONNECTED) {
-  //  delay(500);
-   // Serial.print(".");
- // }
- // Serial.println("");
-  //Serial.print("Connected to ");
-  //Serial.println(ssid);
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
-
-  server.on("/", [](){
-    server.send(200, "text/html", page);
-  });
-  server.on("/kaffee", [](){
-    get_kaffe_simple();
-    delay(1000);
-  });
-  server.on("/remaining_cups", [](){
-    get_cups();
-    delay(1000);
-  });
-  server.on("/raw", [](){
-    get_raw();
-    delay(1000);
-  });
-
-  server.begin();
-  //Serial.println("Web server started!");
-}
-
-void loop()
-{
-  server.handleClient();
-
-  if (WiFi.status() != WL_CONNECTED)
-    connect_to_wifi();
-}
-
-int get_adc()
-{
-  int adc_val_sum = 0;
-  int adc_val2_sum = 0;
-  for (int j=0; j<10;j++)
-  {
-    adc_val_sum = 0;
-    for (int i=0; i<100; i++)
+    void setup()
     {
-     int adc_val = analogRead(SENSOR);
-     adc_val_sum += adc_val;
-     //Serial.print("ADC ");
-     //Serial.print(i+1);
-     //Serial.print(": ");
-     //Serial.println(adc_val);
-     delay(1);
-    }
-    int adc_val_mean = adc_val_sum / 100;
-    adc_val2_sum += adc_val_mean;
-    //Serial.print("Mean of last 100 ADC Values: ");
-    //Serial.println(adc_val_mean);
-  }
-  int adc_val2_mean = adc_val2_sum / 10;
-  //Serial.print("Mean of the last 10 mean Values: ");
-  //Serial.println(adc_val2_mean);
+      Serial.begin(SERIAL_BAUD);
 
-  if (adc_val2_mean <= 620)
-    return adc_val2_mean;
-  else
-    get_adc();
-}
+      line.begin();
+      line.clear();
+      line.show();
+      Serial.println("");
 
-int get_adc_raw()
-{
-  int adc_val_sum = 0;
-  int adc_val2_sum = 0;
-  for (int j=0; j<10;j++)
-  {
-    adc_val_sum = 0;
-    for (int i=0; i<100; i++)
-    {
-     int adc_val = analogRead(SENSOR);
-     adc_val_sum += adc_val;
-     //Serial.print("ADC ");
-     //Serial.print(i+1);
-     //Serial.print(": ");
-     //Serial.println(adc_val);
-     delay(1);
-    }
-    int adc_val_mean = adc_val_sum / 100;
-    adc_val2_sum += adc_val_mean;
-    //Serial.print("Mean of last 100 ADC Values: ");
-    //Serial.println(adc_val_mean);
-  }
-  int adc_val2_mean = adc_val2_sum / 10;
-  //Serial.print("Mean of the last 10 mean Values: ");
-  //Serial.println(adc_val2_mean);
+      connect_to_wifi();
 
-  return adc_val2_mean;
-}
+      //OTA SETUP
+      ArduinoOTA.setPort(OTA_PORT);
+      ArduinoOTA.setHostname(NAME);
+      ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
 
-void get_kaffe_simple()
-{
-  int val = get_adc();
+      ArduinoOTA.onStart([]() {
+        Serial.println("Starting");
+      });
+      ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+      });
+      ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      });
+      ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      });
+      ArduinoOTA.begin();
 
-   if (val >= KANNE_VOLL)
-   {
-      server.send(200, "text/html", "<h1>Kaffee ist voll.</h1><p><a href=\"kaffee\"><button>Fuellstand</button></a>&nbsp;</p>");
-   }
-   else if ((val > KANNE_4) && (val < KANNE_VOLL))
-   {
-      server.send(200, "text/html", "<h1>Kaffee ist etwa halb voll.</h1><p><a href=\"kaffee\"><button>Fuellstand</button></a>&nbsp;</p>");
-   }
-   else if ((val > KANNE_LEER) && (val < KANNE_4))
-   {
-      server.send(200, "text/html", "<h1>Kaffee ist bald leer.</h1><p><a href=\"kaffee\"><button>Fuellstand</button></a>&nbsp;</p>");
-   }
-   else if ((val <= KANNE_LEER) && (val > KANNE_WEG))
-   {
-      server.send(200, "text/html", "<h1>Kaffee ist leer.</h1><p><a href=\"kaffee\"><button>Fuellstand</button></a>&nbsp;</p>");
-   }
-   else if (val <= KANNE_WEG)
-   {
-      server.send(503, "text/html", "<h1>Coffeepot not found</h1><p><a href=\"kaffee\"><button>Fuellstand</button></a>&nbsp;</p>");
-   }
-   return;
-}
+      server.on("/remaining_cups", [](){
+        cups = get_remaining_cups(get_scale_voltage());
+        server.send(200, "text/plain", (((String)cups) + "\n"));
+        Serial.println(cups);
+        set_led_bar(cups, 2);
+        delay(1000);
+      });
+      server.on("/raw", [](){
+        server.send(200, "text/plain", (((String)get_scale_voltage()) + "\n"));
+        delay(1000);
+      });
 
-void get_cups()
-{
-  int val = get_adc();
+      server.begin();
 
-  if (val <= KANNE_WEG)
-  {
-    server.send(503, "text/plain", "Coffeepot not found\n");
-  }
-  else
-  {
-    if (val >= KANNE_VOLL)
-    {
-      server.send(200, "text/plain", "7.0\n");
+      Serial.println("Setup Done!");
     }
-    else if (val >= KANNE_05)
-    {
-      server.send(200, "text/plain", "6.5\n");
-    }
-    else if (val >= KANNE_1)
-    {
-      server.send(200, "text/plain", "6.0\n");
-    }
-    else if (val >= KANNE_15)
-    {
-      server.send(200, "text/plain", "5.5\n");
-    }
-    else if (val >= KANNE_2)
-    {
-      server.send(200, "text/plain", "5.0\n");
-    }
-    else if (val >= KANNE_25)
-    {
-      server.send(200, "text/plain", "4.5\n");
-    }
-    else if (val >= KANNE_3)
-    {
-      server.send(200, "text/plain", "4.0\n");
-    }
-    else if (val >= KANNE_35)
-    {
-      server.send(200, "text/plain", "3.5\n");
-    }
-    else if (val >= KANNE_4)
-    {
-      server.send(200, "text/plain", "3.0\n");
-    }
-    else if (val >= KANNE_45)
-    {
-      server.send(200, "text/plain", "2.5\n");
-    }
-    else if (val >= KANNE_5)
-    {
-      server.send(200, "text/plain", "2.0\n");
-    }
-    else if (val >= KANNE_55)
-    {
-      server.send(200, "text/plain", "1.5\n");
-    }
-    else if (val >= KANNE_6)
-    {
-      server.send(200, "text/plain", "1.0\n");
-    }
-    else if (val >= KANNE_65)
-    {
-      server.send(200, "text/plain", "0.5\n");
-    }
-    else
-    {
-      server.send(200, "text/plain", "0.0\n");
-    }
-  }
-}
 
-void get_raw()
-{
-  server.send(200, "text/plain", (((String)get_adc_raw()) + "\n"));
-}
+    void loop()
+    {
+      server.handleClient(); //do webserver stuff
 
-void connect_to_wifi()
-{
-  WiFi.disconnect();
-  WiFi.begin(WIFI_NAME, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-  }
-}
+      now = millis();
+
+      if (WiFi.status() != WL_CONNECTED) //reconnect if wifi lost
+        connect_to_wifi();
+
+      if (now - last_change > TIMER_S*1000)//update LED bar from time to time
+      {
+        if (raw_adc() >= ADC_PUMP);
+        else
+        {
+          cups = get_remaining_cups(get_scale_voltage());
+          set_led_bar(cups, 2);
+          last_change = now;
+          delay(TIMER_S*10);
+        }
+      }
+
+      if (raw_adc() >= ADC_PUMP)
+      {
+        //show remaining cups on LED
+        set_led_bar(cups, 1);
+      }
+      else if ((get_scale_voltage() <= ADC_EMPTY) && (get_scale_voltage() > ADC_GONE))//red light if coffee is empty
+      {
+        for (int i = 0; i < LED_COUNT; i++)
+          line.setPixelColor(i, line.Color(255/8, 0, 0));
+        line.show();
+      }
+      else
+      {
+       //line.clear();
+       //line.show();
+       set_led_bar(cups, 2);
+     }
+
+    }
+
+
+    //#########################################################################
+    //Functions
+
+    void connect_to_wifi()//(re)connects to wifi and does fancy LED stuff
+    {
+      WiFi.disconnect();
+      WiFi.config(ip, dns, gateway, subnet);
+      WiFi.begin(WIFI_SSID, WIFI_PASS);
+      while (WiFi.status() != WL_CONNECTED)
+      {
+        for(int i=0; i<LED_COUNT; i++)
+        {
+          line.setPixelColor(i, line.Color(0, 0, 255/LED_BRIGHTNESS_DIV));
+          line.show();
+          delay(250/LED_COUNT);
+        }
+        for(int i=0; i<LED_COUNT; i++)
+        {
+          line.setPixelColor(i, line.Color(0,0,0));
+          line.show();
+          delay(250/LED_COUNT);
+        }
+      }
+      Serial.println(WiFi.localIP());
+      line.clear();
+      line.show();
+    }
+
+    int raw_adc()//because Im lazy
+    {
+      return analogRead(SCALE_PIN);
+    }
+
+    void set_led_bar(float _cups, int div)//does Rainboweffect on LED strip
+    {
+
+      int end_led = (int) ((_cups/MAX_CUPS) * LED_COUNT);
+
+      for (int i = 0; i < end_led; i++)
+      {
+        int red = (int) (255 - 255 * ((float) i / LED_COUNT));
+        int green = (int) (255 * ((float) i / LED_COUNT));
+
+        line.setPixelColor(i, line.Color(red/(LED_BRIGHTNESS_DIV*div), green/(LED_BRIGHTNESS_DIV*div), 0));
+      }
+      for (int i = end_led; i < LED_COUNT; i++)
+      {
+        line.setPixelColor(i, line.Color(0, 0, 0));
+      }
+      line.show();
+    }
+
+    //############################################################
+    //functions to get correct amount of Cups
+
+    int get_scale_voltage()
+    {
+      // We take 11 values because the median point is at index 6.
+      int measured_values[11];
+      for (uint8_t index=0; index < 10; index++) {
+        measured_values[index] = raw_adc();
+        delay(1);
+      }
+      // Calculate the array length.
+      int array_length = sizeof(measured_values) / sizeof(measured_values[0]);
+      // Sort the array.
+      qsort(measured_values, array_length, sizeof(measured_values[0]), sort_asc);
+      // Get the median value.
+      //Serial.println(measured_values[5]);
+      return measured_values[5];
+    }
+
+    int sort_asc(const void *cmp1, const void *cmp2)
+    {
+      // Need to cast the void * to int *
+      int a = *((int *)cmp1);
+      int b = *((int *)cmp2);
+      // The comparison
+      return a > b ? 1 : (a < b ? -1 : 0);
+    }
+
+    float get_remaining_cups(int voltage) {
+      // Init remaining cups with the maximum number of cups.
+      float remaining_cups = MAX_CUPS;
+      // Iterate over the voltage array and compare the values.
+      for (int i = 0; i < sizeof(voltages); i++)
+      {
+        if (voltage >= voltages[i]) {
+          remaining_cups = a_cups[i];
+          return remaining_cups;
+        }
+      }
+      // The coffeepot is gone...
+      return -1.0;
+    }
